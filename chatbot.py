917d83db20b3f4b7f6fcf28b54d4b034543535da
Laaaaksh <47391874+Laaaaksh@users.noreply.github.com>
2020-09-08 22:16:12 +0530

    Updated the chatbot for default response

--- chatbot.py
+++ chatbot.py
@@ -4,20 +4,28 @@ from chatterbot.trainers import ChatterBotCorpusTrainer
 
 # Creating Instance
 chatbot = ChatBot(
-    'CoronaBot',
-    storage_adapter='chatterbot.storage.SQLStorageAdapter',
+    'EdzeetaBot',
     logic_adapters=[
-        'chatterbot.logic.MathematicalEvaluation',
-        'chatterbot.logic.TimeLogicAdapter',
-        'chatterbot.logic.BestMatch',
+
         {
             'import_path': 'chatterbot.logic.BestMatch',
-            'default_response': 'Will get back to you on call to clear this query!',
-            'maximum_similarity_threshold': 0.90
+            'maximum_similarity_threshold': 0.65,
+            'default_response': 'Sorry , I dont know this, you can mail my team and get it clear:)',
+
         }
     ],
+
+    input_adapter="chatterbot.input.VariableInputTypeAdapter",
+    output_adapter="chatterbot.output.OutputAdapter",
+    storage_adapter='chatterbot.storage.SQLStorageAdapter',
     database_uri='sqlite:///database.sqlite3'
 ) 
+
+def get_bot_response():
+    userText = request.args.get('msg')
+    botReply = str(bot.get_response(userText))
+    if botReply == "default_value":
+        botReply = str(bot.get_response('default_response'))
 
 training_data_quesans = open('training_data/ques_ans.txt').read().splitlines()
 training_data_personal = open('training_data/personal_ques.txt').read().splitlines()
@@ -29,6 +37,5 @@ trainer.train(training_data)
 
 # Training
 trainer_corpus = ChatterBotCorpusTrainer(chatbot)
-trainer_corpus.train(
-    'chatterbot.corpus.english'
-) 
+trainer_corpus.train('chatterbot.corpus.english') 
+trainer.train("chatterbot.corpus.english.conversations")
