473cac88c10b7c14b5cbc0d4424bcca8661bce3c
yashverma9 <53463512+yashverma9@users.noreply.github.com>
2020-10-26 17:48:23 +0530

    Refreshed db, added new slots

--- init_db.py
+++ init_db.py
@@ -20,22 +20,48 @@ db.session.commit()
 
 
 #Write details for slots
-slot1 = Time_slot(slotDate = "25,Oct", slotTime = "12:30pm", totalSlots = 11, availableSlots = 5)
+slot1 = Time_slot(slotDate = "25,Oct", slotTime = "12:30pm", totalSlots = 11, availableSlots = 11)
 db.session.add(slot1)
 slot2 = Time_slot(slotDate = "25,Oct", slotTime = "02:30pm", totalSlots = 10, availableSlots = 10)
 db.session.add(slot2)
-slot3 = Time_slot(slotDate = "26,Oct", slotTime = "04:30pm", totalSlots = 5, availableSlots = 5)
+
+
+slot3 = Time_slot(slotDate = "26,Oct", slotTime = "04:30pm", totalSlots = 15, availableSlots = 15)
 db.session.add(slot3)
-slot4 = Time_slot(slotDate = "26,Oct", slotTime = "06:00pm", totalSlots = 5, availableSlots = 5)
+slot4 = Time_slot(slotDate = "26,Oct", slotTime = "06:00pm", totalSlots = 25, availableSlots = 25)
 db.session.add(slot4)
-slot5 = Time_slot(slotDate = "27,Oct", slotTime = "01:30pm", totalSlots = 6, availableSlots = 6)
+slot5 = Time_slot(slotDate = "26,Oct", slotTime = "08:00pm", totalSlots = 35, availableSlots = 35)
 db.session.add(slot5)
-slot6 = Time_slot(slotDate = "27,Oct", slotTime = "03:30pm", totalSlots = 7, availableSlots = 7)
+
+slot6 = Time_slot(slotDate = "27,Oct", slotTime = "01:30pm", totalSlots = 60, availableSlots = 60)
 db.session.add(slot6)
-slot7 = Time_slot(slotDate = "28,Oct", slotTime = "04:30pm", totalSlots = 8, availableSlots = 8)
+slot7 = Time_slot(slotDate = "27,Oct", slotTime = "03:30pm", totalSlots = 20, availableSlots = 20)
 db.session.add(slot7)
-slot8 = Time_slot(slotDate = "29,Oct", slotTime = "07:30pm", totalSlots = 5, availableSlots = 5)
+slot8 = Time_slot(slotDate = "27,Oct", slotTime = "04:30pm", totalSlots = 18, availableSlots = 18)
 db.session.add(slot8)
+slot9 = Time_slot(slotDate = "27,Oct", slotTime = "05:30pm", totalSlots = 15, availableSlots = 15)
+db.session.add(slot9)
+
+slot10 = Time_slot(slotDate = "28,Oct", slotTime = "09:30am", totalSlots = 18, availableSlots = 18)
+db.session.add(slot10)
+slot11 = Time_slot(slotDate = "28,Oct", slotTime = "11:15am", totalSlots = 18, availableSlots = 18)
+db.session.add(slot11)
+slot12 = Time_slot(slotDate = "28,Oct", slotTime = "04:30pm", totalSlots = 28, availableSlots = 28)
+db.session.add(slot12)
+slot13 = Time_slot(slotDate = "28,Oct", slotTime = "06:30pm", totalSlots = 23, availableSlots = 23)
+db.session.add(slot13)
+
+
+slot14 = Time_slot(slotDate = "29,Oct", slotTime = "12:30pm", totalSlots = 15, availableSlots = 15)
+db.session.add(slot14)
+slot15 = Time_slot(slotDate = "29,Oct", slotTime = "01:30pm", totalSlots = 15, availableSlots = 15)
+db.session.add(slot15)
+slot16 = Time_slot(slotDate = "29,Oct", slotTime = "02:30pm", totalSlots = 25, availableSlots = 25)
+db.session.add(slot16)
+slot17 = Time_slot(slotDate = "29,Oct", slotTime = "04:45pm", totalSlots = 12, availableSlots = 12)
+db.session.add(slot17)
+slot18 = Time_slot(slotDate = "29,Oct", slotTime = "07:30pm", totalSlots = 10, availableSlots = 10)
+db.session.add(slot18)
 
 db.session.commit()
 
